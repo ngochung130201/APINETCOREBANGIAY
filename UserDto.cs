a802ea1f64bdb493dd82af605d47b7f1d09faf29
HuynhNgocHung <huynhngochungIT@gmail.com.vn>
2022-11-19 18:31:13 +0700

    fix cart user

--- UserDto.cs
+++ UserDto.cs
@@ -4,5 +4,6 @@
     {
         public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public bool IsAdmin { get; set; }
     }
 }
